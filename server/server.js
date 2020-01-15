a9f23276d27933148f18bbe8db025f67df436861
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-01-15 14:46:17 -0500

    [+] Express > Routes: HTTP (GET, POST, PUT, DELETE)

--- server/server.js
+++ server/server.js
@@ -1,9 +1,35 @@
 const express = require( 'express' ),
       app = express();
 
+/** Middleware */
+app .use( express .json() );                            // Analiza las solicitudes entrantes con cargas JSON
+app .use( express .urlencoded({ extended: true }));     // para analizar application/x-www-form-urlencoded
+
 /** Routes */
-app .get( '/', ( request, response ) => {
-    response .json({ 'message': 'Hello World' });
+app .get( '/usuario', ( request, response ) => {
+    response .json({ 'message': 'get /usuario' });
+});
+
+app .post('/usuario', ( request, response ) => {
+    response .json({ 
+        'message': 'put /usuario',
+        'user': request.body
+    });
+});
+
+app .put( '/usuario/:id', ( request, response ) => {
+    let user_id = request .params .id;
+    response .json({ 
+        'message': 'post /usuario',
+        user_id  
+    });
+});
+
+app .delete( '/usuario/:id', ( request, response ) => {
+    response .json({ 
+        'message': 'delete /usuario',
+        'user_id': request .params .id  
+    });
 });
 
 /** Lanza el Servidor */
