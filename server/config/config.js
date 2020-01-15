c8bc2476ccf6bdd6f530259843120a9304c6ae11
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-01-15 15:36:29 -0500

    [+] Node: Configuracion Global de la Aplicacion

---
+++ server/config/config.js
@@ -0,0 +1,2 @@
+/** Configuración Global de la aplicación */
+process .env .PORT = process .env .PORT || 3000;
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require ( './config/config' );
+
 const express = require( 'express' ),
       app = express();
 
@@ -44,6 +46,6 @@ app .delete( '/usuario/:id', ( request, response ) => {
 });
 
 /** Lanza el Servidor */
-app .listen( 3000, () => {
-    console .log( 'Servidor escuchando en el puerto ', 3000 );
+app .listen( process .env .PORT, () => {
+    console .log( 'Servidor escuchando en el puerto:', process .env .PORT );
 });
