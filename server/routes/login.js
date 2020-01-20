0a156afb4ba6d8449b638fc9ba2d966499acd3ad
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-01-20 16:04:53 -0500

    [+] POST: Login Usuario

--- server/routes/login.js
+++ server/routes/login.js
@@ -6,9 +6,47 @@ const express = require( 'express' ),
       User = require( '../models/usuario' );    // Importa el modelo de Usuario
 
 app .post( '/login', ( request, response ) => {
-    response .json({
-        success: true
+    let { email, password } = request .body;
+
+    User .findOne({
+        email
+    }, ( error , usuarioDB ) => {
+        if( error ) {
+            return response .status( 500 ) .json({  /** NOTA: usar el return hace que salga (Finalice el registro de datos) y evita que deba rescribir un else */
+                success: false,
+                error
+            });
+        }
+
+        /** Valida que el Usuario no Existe en la BD */
+        if( ! usuarioDB ) {
+            return response .status( 400 ) .json({  /** NOTA: usar el return hace que salga (Finalice el registro de datos) y evita que deba rescribir un else */
+                success: false,
+                error: {
+                    message: '(Usuario) o Contraseña incorrectos!'
+                }
+            });
+        }
+
+        /** Valida que la contraseña no haga Match */
+        if( ! bcrypt.compareSync( password, usuarioDB .password ) ) {
+            return response .status( 400 ) .json({  /** NOTA: usar el return hace que salga (Finalice el registro de datos) y evita que deba rescribir un else */
+                success: false,
+                error: {
+                    message: 'Usuario o (Contraseña) incorrectos!'
+                }
+            });
+        }
+
+        /** Ejecuta siempre que no exista un error */
+        response .json({        // Cuando no se coloca el status Node asume implicitamente que fue exitosa la petición y el estado será 200 por defecto
+            success: true,
+            token: '',
+            user: usuarioDB
+        });
+
     });
+
 });
 
 
