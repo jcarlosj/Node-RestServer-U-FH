2c2f0a128285eccf57686fabf5fc4d5f123acfce
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-01-22 12:05:47 -0500

    [+] Google Sign-In: Registra usuario usando Credenciales de Google

--- server/routes/login.js
+++ server/routes/login.js
@@ -91,20 +91,106 @@ async function verify( token ) {       // Regresa una Promesa por que es una fun
     }
 } 
 
-/** URI que Obtendrá el Token de Google */
+/** Registra / Autentica usuarios usando Google Sign-In (Credenciales de Google) */
 app .post( '/google', async ( request, response ) => {
     let googleToken = request .body .idtoken,      // Recibe Token de Google
-    googleUser = await verify( googleToken ) .catch( error => {
+        googleUser = await verify( googleToken ) .catch( error => {
             return response .status( 403 ) .json({
                 success: false,
                 error       
             });
-    });       
+        });       
 
-    response .json({
-        success: true,
-        user: googleUser
+    /** Valida que usuario de Google no se encuentre registrado */
+    User .findOne({ email: googleUser .email }, ( error, usuarioDB ) => {
+        if( error ) {
+            return response .status( 500 ) .json({  /** NOTA: usar el return hace que salga (Finalice el registro de datos) y evita que deba rescribir un else */
+                success: false,
+                error
+            });
+        }
+
+        /** Valida si el usuario se encuentra registrado en la BD */
+        if( usuarioDB ) {
+
+            /** Valida si el usuario NO se ha autenticado por Google */
+            if( ! usuarioDB .google ) {
+                if( error ) {
+                    return response .status( 400 ) .json({  /** NOTA: usar el return hace que salga (Finalice el registro de datos) y evita que deba rescribir un else */
+                        success: false,
+                        error: {
+                            message: 'Debe autenticarse de manera tradicional'
+                        }
+                    });
+                }
+            }
+            else {
+                /** Renueva el TOKEN: JWT H256: Solo si el usuario ya se ha autenticado previamente con su cuenta de Google */
+                let token = jwt .sign({
+                        user: usuarioDB,
+                    }, 
+                        process .env .SEED,        // Key or Seed: Semilla de validación (No olvidar configurar variable de entorno en producción).
+                    {
+                        expiresIn: process .env .TOKEN_EXPIRATION_DATE  
+                        /** seconds minutes hours days */
+                    }
+                );
+
+                response .json({
+                    success: true,
+                    token,
+                    user: usuarioDB
+                });
+
+            }
+        }
+        else {
+            /** El usuario NO existe en la BD */
+            let user = new User();      // Creamos una instancia del Modelo Usuario (Mongoose)
+
+            /** Asignamos los valores que vamos a registrar */
+            user .name = googleUser .name;
+            user .email = googleUser .email;
+            user .img = googleUser .img;
+            user .google = true;
+            user .password = 'este-password-no-se-usa';  // La propiedad esta definida como obligatoria en el Modelo Usuario (Mongoose)
+                                                // Sin embargo este valor no será usado para autenticarse vía Google Sign-In.
+
+            /** Registra al usuario con las credenciales de Google Sign-In en nuestra BD */                                                
+            user .save( ( error, usuarioDB ) => {
+                if( error ) {
+                    return response .status( 500 ) .json({  /** NOTA: usar el return hace que salga (Finalice el registro de datos) y evita que deba rescribir un else */
+                        success: false,
+                        error
+                    });
+                }
+
+                /** Si no existe ERROR:
+                 *  - Registra el nuevo Usuario
+                 *  - Crea el Token y
+                 *  - Retorna los datos 
+                 */
+                let token = jwt .sign({
+                        user: usuarioDB,
+                    }, 
+                        process .env .SEED,        // Key or Seed: Semilla de validación (No olvidar configurar variable de entorno en producción).
+                    {
+                        expiresIn: process .env .TOKEN_EXPIRATION_DATE  
+                        /** seconds minutes hours days */
+                    }
+                );
+
+                return response .json({
+                    success: true,
+                    token,
+                    user: usuarioDB
+                });
+
+            });
+        }
+
     });
+
 });
 
 module .exports = app;
