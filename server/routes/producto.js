0d3b1cdb2d57818a41e6be1d19cb5b5731a09e49
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-01-23 16:41:21 -0500

    [+] CRUD > Productos

--- server/routes/producto.js
+++ server/routes/producto.js
@@ -1,46 +1,184 @@
 const express = require( 'express' ),
-      app = express();
+      app = express()
+      /** Modelos Requeridos */
+      Product = require( '../models/producto' );    // Importa el modelo
 
-/** Muestra todas las categorías */
+/** Muestra todas las productos */
 app .get( '/producto', ( request, response ) => {
-    response .json({
-        success: true,
-        message: 'GET /producto'
-    });
+
+    Product .find({})
+        .exec( ( error, productos ) => {
+            /** Valida Error de Bases de datos */
+            if( error ) {
+                return response .status( 500 ) .json({  /** NOTA: usar el return hace que salga (Finalice el registro de datos) y evita que deba rescribir un else */
+                    success: false,
+                    error
+                });
+            }
+
+            /** Retorna la respuesta (siempre que no ocurra un error) */
+            response .json({
+                success: true,
+                count: productos .length,
+                product: productos
+            });
+
+        });
+
 });
 
-/** Muestra una categoría por ID */
+/** Muestra una producto por ID */
 app .get( '/producto/:id', ( request, response ) => {
-    response .json({
-        success: true,
-        message: 'GET /producto/' + request .params .id
+    let id = request .params .id;
+
+    Product .findOne({
+        _id: id
+    }, ( error, productDB ) => {
+        /** Valida Error de Bases de datos */
+        if( error ) {
+            return response .status( 500 ) .json({  /** NOTA: usar el return hace que salga (Finalice el registro de datos) y evita que deba rescribir un else */
+                success: false,
+                error
+            });
+        }
+
+        /** Retorna la respuesta (siempre que no ocurra un error) */
+        response .json({
+            success: true,
+            product: productDB
+        });
     });
+
 });
 
-/** Crea categoría */
+/** Crea producto */
 app .post( '/producto', ( request, response ) => {
-    response .json({
-        success: true,
-        message: 'POST /producto'
+    const { name, description, unitPrice, available } = request .body,        // Obtenemos los datos enviados de la petición (usando el concepto de Destructuración)
+          product = new Product({
+              name,
+              description, 
+              unitPrice,
+              available
+          });
+
+    /** Resgistra los datos en Mongo DB */
+    product .save( ( error, productDB ) => {
+        /** Valida Error de Bases de datos */
+        if( error ) {
+            return response .status( 500 ) .json({  /** NOTA: usar el return hace que salga (Finalice el registro de datos) y evita que deba rescribir un else */
+                success: false,
+                error
+            });
+        }
+
+        /** Handle: No registra producto */
+        if( ! productDB ) {
+            return response .status( 400 ) .json({
+                success: false,
+                error
+            });
+        }
+
+        /** Retorna la respuesta (siempre que no ocurra un error) */
+        response .json({
+            success: true,
+            product: productDB
+        });
     });
+
 });
 
-/** Actualizar categoría */
-app .put( '/producto/:id', ( request, response ) => {
-    response .json({
-        success: true,
-        message: 'PUT /producto/' + request .params .id
+/** Actualizar producto */
+app .put( '/producto/:id', ( request, response ) => {   // findByIdAndUpdate(id, update, options, callback)
+    let product_id = request .params .id,   // Obtenemos los parámetros enviados (GET)
+        body = request .body;               // Obtenermos los valores enviados (POST)
+
+    Product .findByIdAndUpdate( product_id, body, ( error, productDB ) => {
+        /** Valida Error de Bases de datos */
+        if( error ) {
+            return response .status( 500 ) .json({  /** NOTA: usar el return hace que salga (Finalice el registro de datos) y evita que deba rescribir un else */
+                success: false,
+                error
+            });
+        }
+
+        /** Handle: No registra producto */
+        if( ! productDB ) {
+            return response .status( 400 ) .json({
+                success: false,
+                error: { 
+                    message: 'Producto no encontrado'
+                }
+            });
+        }
+
+        /** Retorna la respuesta (siempre que no ocurra un error) */
+        response .json({
+            success: true,
+            product: productDB
+        });
+
     });
+
 });
 
-/** Elimina Categoría 
- * Solo un Administrador puede borrar una categoría
+/** Actualizar disponibilidad del producto */
+app .patch( '/producto/:id', ( request, response ) => {
+    let product_id = request .params .id,   // Obtenemos los parámetros enviados (GET)
+        changeAvailability = { available: false };
+
+    Product .findByIdAndUpdate( product_id, changeAvailability, ( error, productDB ) => {
+        /** Valida Error de Bases de datos */
+        if( error ) {
+            return response .status( 500 ) .json({  /** NOTA: usar el return hace que salga (Finalice el registro de datos) y evita que deba rescribir un else */
+                success: false,
+                error
+            });
+        }
+
+        /** Ejecuta siempre que no exista un error */
+        response .json({ 
+            success: true,
+            user: productDB
+        });
+
+    });
+
+});
+
+/** Elimina producto 
+ * Solo un Administrador puede borrar una producto
 */
 app .delete( '/producto/:id', ( request, response ) => {
-    response .json({
-        success: true,
-        message: 'DELETE /producto/' + request .params .id
+    let product_id = request .params .id;   // Obtenemos los parámetros enviados (GET)
+
+    Product .findByIdAndRemove( product_id, ( error, productDB ) => {
+        /** Valida Error de Bases de datos */
+        if( error ) {
+            return response .status( 500 ) .json({  /** NOTA: usar el return hace que salga (Finalice el registro de datos) y evita que deba rescribir un else */
+                success: false,
+                error
+            });
+        }
+
+        /** Handle: No registra producto */
+        if( ! productDB ) {
+            return response .status( 400 ) .json({
+                success: false,
+                error: { 
+                    message: 'Producto no encontrado'
+                }
+            });
+        }
+
+        /** Ejecuta siempre que no exista un error */
+        response .json({ 
+            success: true,
+            user: productDB
+        });
+
     });
+
 });
 
 module .exports = app;
