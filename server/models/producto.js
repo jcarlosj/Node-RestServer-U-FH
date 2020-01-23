6398735adee3bed443cf7ff546493713e3b9e955
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-01-23 14:43:11 -0500

    [+] Mongoose: Model 'Producto'

---
+++ server/models/producto.js
@@ -0,0 +1,32 @@
+const mongoose = require( 'mongoose' );
+
+let Schema = mongoose .Schema,
+    productSchema = new Schema({
+        name: {
+            type: String,
+            required: [ true, 'El nombre del producto es requerido' ]
+        },
+        description: {
+            type: String
+        },
+        unitPrice: {
+            type: Number,
+            required: [ true, 'El precio únitario del producto es requerido' ]
+        },
+        available: {
+            type: Boolean,
+            required: true,
+            default: true
+        },
+        category: {
+            type: Schema .Types .ObjectId,
+            ref: 'Categoria'
+        },
+        user: {
+            type: Schema .Types .ObjectId,
+            ref: 'Usuario'
+        },  
+
+    });
+
+module .exports = mongoose .model( 'Producto', productSchema );
